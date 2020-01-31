06de7ca464d24cc2976b04ffe3fd8d525c30454a
Missing-q <ivybetterit@gmail.com>
2020-01-31 17:05:18 -0500

    Setup stuff

---
+++ Universal FE Randomizer/src/fedata/wii/fe10/FE10Class.java
@@ -0,0 +1,11 @@
+package fedata.wii.fe10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fedata.general.FEModifiableData;
+
+public class FE10Class implements FEModifiableData {
+	//haha nothing here yet
+
+}
